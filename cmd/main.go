8f87908e920ca99f5845def32a5c055eb9500648
Shintaro Okamura <okash1n.fcs@gmail.com>
2021-11-07 00:19:45 +0000

    14 bool

--- cmd/main.go
+++ cmd/main.go
@@ -3,28 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var fl64 float64 = 2.4
-	fmt.Println(fl64)
+	var t, f bool = true, false
+	fmt.Println(t, f)
 
-	fl := 3.2
-	fmt.Printf("%T\n", fl)
-	fmt.Println(fl64 + fl)
-	fmt.Printf("%T, %T\n", fl64, fl)
-
-	var fl32 float32 = 1.2
-	fmt.Printf("%T\n", fl32)
-
-	fmt.Printf("%T\n", float64(fl32))
-
-	zero := 0.0
-	fmt.Printf("%T\n", zero)
-
-	pinf := 1.0 / zero
-	fmt.Println(pinf)
-
-	ninf := -1.0 / zero
-	fmt.Println(ninf)
-
-	nan := zero / zero
-	fmt.Println(nan)
 }

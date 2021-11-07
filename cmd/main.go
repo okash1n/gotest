c35789dbeb1d07bdbbaeac6bba97e4080be2c151
Shintaro Okamura <okash1n.fcs@gmail.com>
2021-11-07 00:47:24 +0000

    15 string

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var t, f bool = true, false
-	fmt.Println(t, f)
+
+	var s string = "Hello Golang"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	var si string = "300"
+	fmt.Println(si)
+	fmt.Printf("%T\n", si)
+
+	fmt.Println(`test
+	test
+		test`)
+
+	fmt.Println("\"")
+	fmt.Println(`"`)
+
+	fmt.Println(s[0])
 
 }

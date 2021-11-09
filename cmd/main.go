27ecbf673ca7e8785721e5064e1ce87ecf556992
Shintaro Okamura <okash1n.fcs@gmail.com>
2021-11-09 15:51:04 +0000

    22_type_conversion

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var x interface{}
-	fmt.Println(x)
+	/*
+		var i int = 1
+		fl64 := float64(i)
+		fmt.Println(fl64)
+		fmt.Printf("i = %T\n", i)
+		fmt.Printf("fl64 = %T\n", fl64)
 
-	x = 1
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
-	x = "A"
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
-	x = 3.14
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
-	x = 2
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
+		i2 := int(fl64)
+		fmt.Printf("i2 = %T\n", i2)
+
+		fl := 10.5
+		i3 := int(fl)
+		fmt.Printf("i3 = %T\n", i3)
+		fmt.Println(i3)
+	*/
+
+	var s string = "100"
+	fmt.Println(s)
+	fmt.Printf("s = %T\n", s)
+
+	i, _ := strconv.Atoi(s)
+	fmt.Println(i)
+	fmt.Printf("i = %T\n", i)
+
+	var i2 int = 200
+	s2 := strconv.Itoa(i2)
+	fmt.Println(s2)
+	fmt.Printf("s2 = %T\n", s2)
+
+	var h string = "Hello World"
+	b := []byte(h)
+	fmt.Println(b)
+
+	h2 := string(b)
+	fmt.Println(h2)
 
 }

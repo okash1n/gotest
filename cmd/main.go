1c5ef156643e37fdaf9d8a95adda7c16b5589025
Shintaro Okamura <okash1n.fcs@gmail.com>
2021-11-09 14:07:38 +0000

    array

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	byteA := []byte{72, 73}
-	fmt.Println(byteA)
+	var arr1 [3]int
+	fmt.Println(arr1)
+	fmt.Printf("%T\n", arr1)
 
-	fmt.Println(string(byteA))
+	var arr2 [3]string = [3]string{"A", "B"}
+	fmt.Println(arr2)
 
-	c := []byte("こんにちは")
-	fmt.Println(c)
+	arr3 := [3]int{1, 2, 3}
+	fmt.Println(arr3)
 
-	fmt.Println(string(c))
+	arr4 := [...]string{"C"}
+	fmt.Println(arr4)
+	fmt.Printf("%T\n", arr4)
 
+	fmt.Println(arr2[0])
+	fmt.Println(arr2[1])
+	fmt.Println(arr2[2])
+	fmt.Println(arr2[2-1])
+	fmt.Println(arr2[3-1])
+
+	arr2[2] = "C"
+	fmt.Println(arr2)
+
+	// var arr5 [4]int
+	// arr5 = arr1
+	// fmt.Println(arr5)
+
+	fmt.Println(len(arr1), cap(arr1))
 }

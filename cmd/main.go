12b48e5a12292341653d0e340437f17bb2e5de40
Shintaro Okamura <shintaro.okamura@studist.jp>
2021-11-10 15:04:28 +0000

    29_return_function

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,12 @@ package main
 
 import "fmt"
 
-func main() {
-	f := func(x, y int) int {
-		return x + y
+func ReturnFunc() func() {
+	return func() {
+		fmt.Println("I'm a function")
 	}
-	i := f(1, 2)
-	fmt.Println(i)
-
-	i2 := func(x, y int) int {
-		return x + y
-	}(1, 2)
-	fmt.Println(i2)
+}
+func main() {
+	f := ReturnFunc()
+	f()
 }

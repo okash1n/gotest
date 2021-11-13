81910f8756c8a51c004006d0ce0547de0bae5db7
Shintaro Okamura <shintaro.okamura@studist.jp>
2021-11-13 11:37:17 +0000

    35_if

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,23 @@ package main
 
 import "fmt"
 
-func integers() func() int {
-	i := 0
-	return func() int {
-		i++
-		return i
+func main() {
+	a := 1
+	if a == 2 {
+		fmt.Println("two")
+
+	} else if a == 1 {
+		fmt.Println("one")
+	} else {
+		fmt.Println("I don't know")
 	}
 
-}
-
-func main() {
-	ints := integers()
-	fmt.Println(ints())
-	fmt.Println(ints())
-	fmt.Println(ints())
-	fmt.Println(ints())
-	fmt.Println(ints())
-	fmt.Println(ints())
-
-	otherints := integers()
-	fmt.Println(otherints())
-	fmt.Println(otherints())
-	fmt.Println(otherints())
-	fmt.Println(otherints())
-
+	if b := 100; b == 100 {
+		fmt.Println("one hundred")
+	}
+	x := 0
+	if x := 2; true {
+		fmt.Println(x)
+	}
+	fmt.Println(x)
 }

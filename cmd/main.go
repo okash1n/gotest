ed2d7ab45089c3be020e895d72842c8960b01f74
Shintaro Okamura <shintaro.okamura@studist.jp>
2021-11-13 13:51:16 +0000

    37_for

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,64 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	var s string = "Hello"
-	// var s int = 100
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		fmt.Println("ERROR")
-		fmt.Printf("err = %T\n", err)
-		fmt.Println(err)
-	} else {
-		fmt.Printf("i = %T\n", i)
+	/*
+		i := 0
+		for {
+			i++
+			if i == 10 {
+				break
+			}
+			fmt.Println("Loop")
+		}
+	*/
+
+	/*
+		point := 0
+		for point < 10 {
+			fmt.Println(point)
+			point++
+		}
+	*/
+
+	/*
+		for i := 0; i < 10; i++ {
+			if i == 3 {
+				continue
+			}
+			if i == 6 {
+				break
+			}
+			fmt.Println(i)
+		}
+	*/
+
+	/*
+		arr := [3]int{1, 2, 3}
+		for i := 0; i < len(arr); i++ {
+			fmt.Println(arr[i])
+		}
+	*/
+
+	/*
+
+		arr := [3]int{1, 2, 3}
+		for i, v := range arr {
+			fmt.Println(i, v)
+		}
+	*/
+
+	/*
+		sl := []string{"Python", "PHP", "GO"}
+		for i, v := range sl {
+			fmt.Println(i, v)
+		}
+	*/
+
+	m := map[string]int{"apple": 100, "banana": 200}
+
+	for k, v := range m {
+		fmt.Println(k, v)
 	}
 }
